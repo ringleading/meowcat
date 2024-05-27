a50151752522cae62e1a1bee4ea655a5738a9952
lingreading <wouldlikesomebanter@gmail.com>
2024-05-27 15:26:17 +0800

    Add OKX link

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <span className={styles.repeating}>meow</span>
       <span className={styles.repeating_right}>meow</span>
       <main className="grid h-screen place-items-center text-white">
-        <div className="flex flex-col justify-center text-center gap-5">
+        <div className="flex flex-col justify-center text-center gap-2">
           <Image
             src={sleeping_meowcat}
             alt="meowcat"
@@ -63,6 +63,19 @@ export default function Home() {
             >
               CoinMarketCap
             </a>
+          </div>
+          <div>
+            <p className="uppercase font-bold drop-shadow-md text-green-600 text-xl">
+              We are now live on{" "}
+              <a
+                href="https://www.okx.com/web3/dex-swap#inputChain=43114&inputCurrency=0xb97ef9ef8734c71904d8002f8b6bc66dd9c48a6e&outputChain=43114&outputCurrency=0x8ad25b0083c9879942a64f00f20a70d3278f6187"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="underline hover:cursor-pointer"
+              >
+                OKX
+              </a>
+            </p>
           </div>
 
           <div
